dc9714b84da9e159df6287abacb10a44efbc1fb0
spotdemo4 <racierjet@hotmail.com>
2023-03-29 20:54:54 -0400

    init

---
+++ vpngui.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+from flask import send_from_directory
+from random import randint
+from waitress import serve
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    rand = str(randint(5, 69))
+    qrimg = './config/peer' + rand + '/peer' + rand + '.png'
+    config = './config/peer' + rand + '/peer' + rand + '.conf'
+    return render_template("index.html", qr = qrimg, name = "ny-"+rand, conf = config, location = "New York")
+
+@app.route('/config/<path:filename>')
+def getimage(filename):
+    return send_from_directory(app.root_path + '/config/', filename)
+
+if __name__ == "__main__":
+    serve(app, host="0.0.0.0", port=5070)
